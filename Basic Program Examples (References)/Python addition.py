6b1492b513dc1b10b21bfe179865442ff0b6a5fa
RedEspresso <amanda.inerfeld@gmail.com>
2017-06-01 18:23:49 -0400

    File ReOrg

--- Basic Program Examples (References)/Python addition.py
+++ Basic Program Examples (References)/Python addition.py
@@ -3,8 +3,8 @@ Again = True
 while Again:
     x = int(input("Enter the first number: "))
     y = int(input("Enter the second number to add: "))
-	print (x "plus" y "=" )
-    print(x + y)
+    print (str(x) + " plus " + str(y) + "=" + str(x+y))
+    #print(x + y)
 
 
     if input("Again? ") == "no":
--- Simple Programs/Rolling dice.py
+++ Simple Programs/Rolling dice.py
@@ -5,8 +5,9 @@ import random
 #while rollAgain in ["Yes" , "yes", "y"]:
 diceChoice = input ("Roll the Dice? Y/N")
 if diceChoice in ["Yes" , "yes", "y", "Y"]:
-        print("You rolled a ", random.randint(1,6))
-        print ("Thank you for playing")
+    print("You rolled a ", random.randint(1,6))
+    print("Thank you for playing")
 else:
-      print ("Thank you for playing")
+    print("Thank you for playing")
+
 #TODO: Let user play again
