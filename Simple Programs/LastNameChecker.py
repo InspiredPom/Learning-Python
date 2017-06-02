44cf0bfd85e2e194c0faa43d845b72d6ad2e4351
RedEspresso <amanda.inerfeld@gmail.com>
2017-06-01 21:59:31 -0400

    Checking if input is valid last name

--- Simple Programs/LastNameChecker.py
+++ Simple Programs/LastNameChecker.py
@@ -7,3 +7,7 @@ string = "Am32423423423anda"
 string2 = re.sub('[0-9]', '', string)
 print (string2)
 
+if re.match('([A-Z])[a-z]+$', string):
+    print(string + 'is a valid last name')
+else:
+    print(string + ' is not a valid last name')
