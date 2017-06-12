79bdc5bb6c6f216004d80b378262124fc1367109
RedEspresso <amanda.inerfeld@gmail.com>
2017-06-11 20:48:57 -0400

    Added todo comment

--- Simple Programs/Palidrome.py
+++ Simple Programs/Palidrome.py
@@ -2,16 +2,15 @@
 #break into 2
 #Check if palidrome by seeing if two strings match back and forwards
 string = input("Enter a word, see if it is a palidrome\n")
-result = True
+#result = True
 def is_palindrome(string):
-    result = True
+    #result = True
     str_len = len(string)
-    for i in range(0, int(str_len/2)): # you need to check only half of the string
+    for i in range(0, int(str_len/2)):
         if string[i] != string[str_len-i-1]:
             result = False
             break
     return result
 print ("You Entered:  ",string)
 print (result)
-
-#Define result
+#Todo: Need to find way to not always get true
